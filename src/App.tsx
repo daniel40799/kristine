f7a2df108c0490a6d23a762b6d01d876cd4b9200
Daniel Dalaota <daniel40799@gmail.com>
2026-04-15 19:20:36 +0800

    init part 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const images = ['tin (1).jpg', 'tin (5).jpg', 'tin (6).jpg', 'tin (7).jpg', 'tin (8).jpg'];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello tenten ko manghod nako dodong and kakat</h1>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))', gap: '20px', padding: '20px' }}>
+          {images.map((image) => (
+            <div key={image} style={{ textAlign: 'center' }}>
+              <img src={`/${image}`} alt={image} style={{ maxWidth: '100%', height: 'auto' }} />
+            </div>
+          ))}
+        </div>
+
+          <h1>ako manghod gwapo ako manghod gwapa ako gwapa</h1>
       </header>
     </div>
   );
